63701a17f6847cddd6fed2eb06b4c6892560d971
Jakub Błachut <jakub13blachut@gmail.com>
2023-10-06 18:11:54 +0200

    Add Pistol and shooting system. Remove Teleportation

---
+++ Assets/Script/Shooting.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Shooting : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable weapon = GetComponent<XRGrabInteractable>();
+        weapon.activated.AddListener(Fire);
+    }
+
+    private void Fire(ActivateEventArgs arg0)
+    {
+        GameObject spawnedbullet = Instantiate(bullet);
+        spawnedbullet.transform.position = spawnPoint.position;
+        spawnedbullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedbullet, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
