7dc3c956fa33691d865416eecb4afdde8dd3c938
Jakub Błachut <jakub13blachut@gmail.com>
2023-10-08 22:08:31 +0200

    EnemyMovement
    To player static position

---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float rotationSpeed, move, distanceOfPlayer, fow;
+    private float distance;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void  FixedUpdate()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance < fow)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.position - player.transform.position), rotationSpeed * Time.deltaTime);
+
+            if (distance >= distanceOfPlayer)
+            {
+                transform.position += -transform.forward * move * Time.deltaTime;
+            }
+        }
+
+    }
+}
---
+++ Assets/Script/EnemyShoot.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyShoot : MonoBehaviour
+{
+    
+    
+    [SerializeField] private float timer = 5;
+    private float bulletTime;
+
+    public GameObject enemyBullet;
+    public Transform spawnEnemyBulletPoint;
+    public float enemySpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        ShootAtPlayer();
+    }
+
+    void ShootAtPlayer()
+    {
+        bulletTime -= Time.deltaTime;
+        if (bulletTime > 0) return;
+        bulletTime = timer;
+        GameObject bulletObj = Instantiate(enemyBullet, spawnEnemyBulletPoint.transform.position, spawnEnemyBulletPoint.transform.rotation) as GameObject;
+        Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
+        bulletRig.AddForce(bulletRig.transform.forward * enemySpeed);
+        Destroy(bulletObj, 5f);
+
+    }
+}
