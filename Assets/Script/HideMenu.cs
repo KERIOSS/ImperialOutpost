efa849d145fd9574bbdedce40b901a29a48d47db
Jakub Błachut <jakub13blachut@gmail.com>
2023-12-13 15:06:52 +0100

    Menu upgrade
    The system of hiding hands in the menu

--- Assets/Script/HideMenu.cs
+++ Assets/Script/HideMenu.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class HideMenu : MonoBehaviour
 {
     public GameObject menu;
-    
+    public GameObject sword;
+    public GameObject pistol;
+    public GameObject lh;
+    public GameObject rh;
+
     public void Hide()
 	{
         menu.SetActive(false);
         Time.timeScale = 1;
+        sword.SetActive(true);
+        pistol.SetActive(true);
+        lh.SetActive(false);
+        rh.SetActive(false);
 
-        
 	}
 
 }
